e3d88183e65f6b28f208da5193e8f91cbf0e2be5
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-01-06 12:30:29 +0300

    fix(login): fixed bg/text colors on login page

--- frontend/src/components/layout/SiteGradient/index.tsx
+++ frontend/src/components/layout/SiteGradient/index.tsx
@@ -6,7 +6,7 @@ interface IProps {
 
 export default function SiteGradient({ children }: IProps) {
   return (
-    <div className="p-5 h-fit flex flex-col items-center justify-center bg-gradient bg-gradient-to-b from-mygrad-start to-mygrad-end">
+    <div className="p-5 h-fit min-h-screen flex flex-col items-center justify-center bg-gradient bg-gradient-to-b from-mygrad-start to-mygrad-end">
       {children}
     </div>
   );
